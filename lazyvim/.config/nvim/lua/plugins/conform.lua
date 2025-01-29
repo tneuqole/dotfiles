72417bf72196eb8e21636c6aac3773ce0d2a0127
tneuqole <woodmanjd32@gmail.com>
2025-01-28 21:22:43 -0500

    sync

--- lazyvim/.config/nvim/lua/plugins/conform.lua
+++ lazyvim/.config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,8 @@ return {
   opts = {
     formatters_by_ft = {
       sql = { "sql_formatter" },
+      -- html = { "prettierd", "prettier" },
+      html = { "prettier" },
     },
     formatters = {
       sql_formatter = {
